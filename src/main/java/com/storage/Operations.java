45300b7428825c90636e7e0583143e6baaa4ad8c
Lazar Dejanovic <ldejanovic8120rn@raf.rs>
2021-10-30 18:51:34 +0200

    Specifikacija - GMTA

--- src/main/java/com/storage/Operations.java
+++ src/main/java/com/storage/Operations.java
@@ -1,29 +1,76 @@
 package com.storage;
 
+import com.utils.FileMetadata;
 import com.utils.Privilege;
 import com.utils.StorageInfo;
 
+import java.util.Comparator;
+import java.util.Date;
 import java.util.List;
 
 public abstract class Operations {
 
     // TODO - Za sve metode proveriti da li je user logovan
-    public abstract List<String> getAllFiles(String path);
-    public abstract List<String> getAllDirectories(String path);
-    public abstract List<String> getAllFilesRecursive(String path);
-    public abstract List<String> getAllFilesWithExtention(String path, String extention);
-    public abstract List<String> getSortedBy(String path, String criteria);
-    public abstract List<String> getInBetweenDates(String path, String startDate, String endDate);
-    public abstract void copyFile(String fromPath, String toPath, String fileName);
+    public abstract List<FileMetadata> getAllFiles(String path);
+    public abstract List<FileMetadata> getAllDirectories(String path);
+    public abstract List<FileMetadata> getAllFilesRecursive(String path);
+    public abstract void uploadFile();
+    public abstract void moveFile(String fromPath, String toPath, String fileName);
 
-    public void copyFiles(String fromPath, String toPath, List<String> fileNames) throws Exception {
+    public void moveFiles(String fromPath, String toPath, List<String> fileNames) throws Exception {
         if (!StorageInfo.getStorageInfo().checkUser(Privilege.ADMIN, Privilege.RDCD)) {
             throw new Exception("Korisnik nije logovan ili nema privilegiju");
         }
 
         for(String fileName: fileNames) {
-            copyFile(fromPath, toPath, fileName);
+            moveFile(fromPath, toPath, fileName);
         }
     }
 
+    public List<FileMetadata> getAllFilesWithExtention(List<FileMetadata> files, String extention) {
+        // TODO - ako ne radi izbacivanje zadate ekstenzije
+        files.removeIf(file -> !file.getName().endsWith(extention));
+        return files;
+    }
+
+    // criteria - naziv, datum kreiranja i modifikacija
+    public List<FileMetadata> getSortedBy(List<FileMetadata> files, String criteria) {
+        if (criteria.equals("sortByName")) {
+            sortByName(files);
+        }
+
+        if (criteria.equals("sortByDate")) {
+            sortByDate(files);
+        }
+
+        if (criteria.equals("sortByModification")) {
+            sortByModification(files);
+        }
+
+        return files;
+    }
+
+    public List<FileMetadata> getInBetweenDates(List<FileMetadata> files, Date startDate, Date endDate) {
+        files.removeIf(file -> !(file.getCreationDate().after(startDate) && file.getCreationDate().before(endDate)));
+        return files;
+    }
+
+    private void sortByName(List<FileMetadata> files) {
+        // TODO - ako ne radi sortiranje
+        files.sort(Comparator.comparing(FileMetadata::getName));
+//        files.sort(new Comparator<FileMetadata>() {
+//            @Override
+//            public int compare(FileMetadata o1, FileMetadata o2) {
+//                return o1.getName().compareTo(o2.getName());
+//            }
+//        });
+    }
+
+    private void sortByDate(List<FileMetadata> files) {
+        files.sort(Comparator.comparing(FileMetadata::getCreationDate));
+    }
+
+    private void sortByModification(List<FileMetadata> files) {
+        files.sort(Comparator.comparing(FileMetadata::getModificationDate));
+    }
 }
---
+++ src/main/java/com/utils/FileMetadata.java
@@ -0,0 +1,28 @@
+package com.utils;
+
+import java.util.Date;
+
+public class FileMetadata {
+
+    private String name;
+    private Date creationDate;
+    private Date modificationDate;
+
+    public FileMetadata(String name, Date creationDate, Date modificationDate) {
+        this.name = name;
+        this.creationDate = creationDate;
+        this.modificationDate = modificationDate;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Date getCreationDate() {
+        return creationDate;
+    }
+
+    public Date getModificationDate() {
+        return modificationDate;
+    }
+}
