9758255abef8d0938326b2dc6f3ac91abdfbcb59
vvukovic9420rn <vvukovic9420rn@raf.rs>
2021-11-16 22:55:12 +0100

    javadoc complete

--- src/main/java/com/storage/Delete.java
+++ src/main/java/com/storage/Delete.java
@@ -13,10 +13,31 @@ import java.util.List;
  */
 public abstract class Delete {
 
+    /**
+     * Method that deletes directory at given path.
+     * @param directoryName path to directory
+     * @throws Exception if user isn't logged or doesn't have privilege
+     */
     public abstract void deleteDirectory(String directoryName) throws Exception;
+
+    /**
+     * Method that deletes file at given path
+     * @param fileName path to file
+     * @throws Exception if user isn't logged or doesn't have privilege
+     */
     public abstract void deleteFile(String fileName) throws Exception;
+
+    /**
+     * Method that deletes everything in storage
+     * @throws Exception if user isn't logged or doesn't have privilege
+     */
     public abstract void deleteAll(String rootPath) throws Exception;
 
+    /**
+     * Method that deletes directory at given path.
+     * @param directoryName path to directory
+     * @throws Exception if user isn't logged or doesn't have privilege
+     */
     public void removeDirectory(String directoryName) throws Exception {
         if (!StorageInfo.getStorageInfo().checkUser(Privilege.ADMIN, Privilege.RDCD)) {
             throw new LogException("User isn't logged or doen't have permission");
@@ -25,6 +46,11 @@ public abstract class Delete {
         deleteDirectory(directoryName);
     }
 
+    /**
+     * Method that deletes multiple directories at given paths.
+     * @param directoryNames List of paths to directories
+     * @throws Exception if user isn't logged or doesn't have privilege
+     */
     public void removeDirectories(List<String> directoryNames) throws Exception {
         if (!StorageInfo.getStorageInfo().checkUser(Privilege.ADMIN, Privilege.RDCD)) {
             throw new LogException("User isn't logged or doen't have permission");
@@ -36,6 +62,11 @@ public abstract class Delete {
 
     }
 
+    /**
+     * Method that deletes file at given path
+     * @param fileName path to file
+     * @throws Exception if user isn't logged or doesn't have privilege
+     */
     public void removeFile(String fileName) throws Exception {
         if (!StorageInfo.getStorageInfo().checkUser(Privilege.ADMIN, Privilege.RDCD)) {
             throw new LogException("User isn't logged or doen't have permission");
@@ -44,6 +75,11 @@ public abstract class Delete {
         deleteFile(fileName);
     }
 
+    /**
+     * Method that deletes multiple files at given path for each file
+     * @param fileNames List of file paths
+     * @throws Exception if user isn't logged or doesn't have privilege
+     */
     public void removeFiles(List<String> fileNames) throws Exception {
         if (!StorageInfo.getStorageInfo().checkUser(Privilege.ADMIN, Privilege.RDCD)) {
             throw new LogException("User isn't logged or doen't have permission");
@@ -54,6 +90,10 @@ public abstract class Delete {
         }
     }
 
+    /**
+     * Method that deletes everything in storage
+     * @throws Exception if user isn't logged or doesn't have privilege
+     */
     public void removeAll() throws Exception {
         if (!StorageInfo.getStorageInfo().checkUser(Privilege.ADMIN, Privilege.RDCD)) {
             throw new LogException("User isn't logged or doen't have permission");
--- src/main/java/com/storage/Operations.java
+++ src/main/java/com/storage/Operations.java
@@ -15,19 +15,76 @@ import java.util.List;
  */
 public abstract class Operations {
 
+    /**
+     * Method that gets all files at given path.
+     * @param path Path within storage
+     * @return List of FileMetada at path
+     * @see FileMetadata
+     * @throws Exception if user isn't logged or path doesn't exist
+     */
     public abstract List<FileMetadata> getAllFiles(String path) throws Exception;
+
+    /**
+     * Method that lists all directories at given path
+     * @param path Path within storage
+     * @return List of FileMetada at path
+     * @see FileMetadata
+     * @throws Exception if user isn't logged or path doesn't exist
+     */
     public abstract List<FileMetadata> getAllDirectories(String path) throws Exception;
+
+    /**
+     * Method that gets all files at given path, including files within other directories.
+     * @param path Path within storage
+     * @return List of FileMetada at path
+     * @see FileMetadata
+     * @throws Exception if user isn't logged or path doesn't exist
+     */
     public abstract List<FileMetadata> getAllFilesRecursive(String path) throws Exception;
+
+    /**
+     * Method that downloads a file.
+     * @param path Path to file
+     * @throws Exception if user isn't logged or doesn't have privilege or path doesn't exist
+     */
     public abstract void download(String path) throws Exception;
+
+    /**
+     * Method that uploads given file to specified path.
+     * @param fromPath Path to file that should be uploaded
+     * @param toPath Path to which given file should be uploaded
+     * @throws Exception if user isn't logged or doesn't have privilege or path doesn't exist
+     */
     public abstract void uploadFile(String fromPath, String toPath) throws Exception;
+
+    /**
+     * Method that moves given file to specified path.
+     * @param fromPath Path to file that should be moved
+     * @param toPath Path to which given file should be moved to
+     * @throws Exception if user isn't logged or doesn't have privilege or path doesn't exist
+     */
     public abstract void moveFile(String fromPath, String toPath) throws Exception;
 
+    /**
+     * Method that filters files by given extension.
+     * @param files List of FileMetadatas
+     * @param extention File extension that should be searched for
+     * @return FileMetadata list with given extension
+     * @see FileMetadata
+     */
     public List<FileMetadata> getAllFilesWithExtention(List<FileMetadata> files, String extention) {
         // TODO - ako ne radi izbacivanje zadate ekstenzije
         files.removeIf(file -> !file.getName().endsWith(extention));
         return files;
     }
 
+    /**
+     * Method that sorts files by various criteria.
+     * @param files List of FileMetadatas
+     * @param criteria Criteria for file sorting (sortByName, sortByDate, sortByModification and their combinations)
+     * @return FileMetadata list sorted by given criteria
+     * @see FileMetadata
+     */
     public List<FileMetadata> getSortedBy(List<FileMetadata> files, String criteria) {
         if (criteria.equals("sortByName")) {
             sortByName(files);
@@ -68,6 +125,14 @@ public abstract class Operations {
         return files;
     }
 
+    /**
+     * Method that filters file in between two dates
+     * @param files List of FileMetadatas
+     * @param startDate starting date for filtering
+     * @param endDate ending date for filtering
+     * @return FileMetada filtered list
+     * @see FileMetadata
+     */
     public List<FileMetadata> getInBetweenDates(List<FileMetadata> files, Date startDate, Date endDate) {
         files.removeIf(file -> !(file.getCreationDate().after(startDate) && file.getCreationDate().before(endDate)));
         return files;
@@ -84,26 +149,56 @@ public abstract class Operations {
 //        });
     }
 
+    /**
+     * Method that sorts given FileMetada list by date
+     * @param files FileMetadata list that should be sorted
+     * @see FileMetadata
+     */
     private void sortByDate(List<FileMetadata> files) {
         files.sort(Comparator.comparing(FileMetadata::getCreationDate));
     }
 
+    /**
+     * Method that sorts given FileMetada list by modification date
+     * @param files FileMetadata list that should be sorted
+     * @see FileMetadata
+     */
     private void sortByModification(List<FileMetadata> files) {
         files.sort(Comparator.comparing(FileMetadata::getModificationDate));
     }
 
+    /**
+     * Method that sorts given FileMetada list by name and date
+     * @param files FileMetadata list that should be sorted
+     * @see FileMetadata
+     */
     private void sortByNameAndDate(List<FileMetadata> files) {
         files.sort(Comparator.comparing(FileMetadata::getName).thenComparing(FileMetadata::getCreationDate));
     }
 
+    /**
+     * Method that sorts given FileMetada list by name and modification date
+     * @param files FileMetadata list that should be sorted
+     * @see FileMetadata
+     */
     private void sortByNameAndModification(List<FileMetadata> files) {
         files.sort(Comparator.comparing(FileMetadata::getName).thenComparing(FileMetadata::getModificationDate));
     }
 
+    /**
+     * Method that sorts given FileMetada list by date and modification date
+     * @param files FileMetadata list that should be sorted
+     * @see FileMetadata
+     */
     private void sortByDateAndModification(List<FileMetadata> files) {
         files.sort(Comparator.comparing(FileMetadata::getCreationDate).thenComparing(FileMetadata::getModificationDate));
     }
 
+    /**
+     * Method that sorts given FileMetada list by all categories
+     * @param files FileMetadata list that should be sorted
+     * @see FileMetadata
+     */
     private void sortByAll(List<FileMetadata> files) {
         files.sort(Comparator.comparing(FileMetadata::getName).thenComparing(FileMetadata::getCreationDate).thenComparing(FileMetadata::getModificationDate));
     }
